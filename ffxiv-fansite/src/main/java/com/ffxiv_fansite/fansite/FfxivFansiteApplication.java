ab6611baa32d3b6acf93815475b02281257e0093
Martijn Jansen <martijnjansen91@gmail.com>
2025-05-20 12:54:40 +0200

    Init

---
+++ ffxiv-fansite/src/main/java/com/ffxiv_fansite/fansite/FfxivFansiteApplication.java
@@ -0,0 +1,13 @@
+package com.ffxiv_fansite.fansite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FfxivFansiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FfxivFansiteApplication.class, args);
+	}
+
+}
---
+++ ffxiv-fansite/src/test/java/com/ffxiv_fansite/fansite/FfxivFansiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ffxiv_fansite.fansite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FfxivFansiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
